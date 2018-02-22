05e9cfd3e7154f0d80c92ba359308c8be5042e4a
15EC039 <ranjithar98@gmail.com>
2018-02-22 12:36:07 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d",&a);
+    scanf("%d",&b);
+while (b!= 0)
+    {
+        c*= a;
+        --b;
+    }
+printf("%d",c);
+    return 0;
+}
